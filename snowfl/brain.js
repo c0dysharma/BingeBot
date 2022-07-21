97e828acde0151401ac852cb488aa527a09c4f5d
Kuldeep <c0dysharma01@gmail.com>
2022-07-21 17:07:13 +0530

    shorten magnet links

--- snowfl/brain.js
+++ snowfl/brain.js
@@ -38,16 +38,24 @@ async function createPage(responseArray) {
   let limit = Math.min(responseArray.length, 50);
   for (let i = 0; i < limit; i++) {
     const stuff = responseArray[i];
-    // stuff.magnet = stuff.magnet ? stuff.magnet : stuff.url;
+    if (stuff.magnet) {
+      const mag = await axios.get(`http://mgnet.me/api/create?m=${stuff.magnet}`)
+      if (mag.data.state == 'success')
+        stuff.magnet = mag.data.shorturl;
+    }
 
     const title = { "tag": "b", "children": [`${stuff.name}\n`] };
     const details = { "tag": "p", "children": [`Type: ${stuff.type} • Age: ${stuff.age}\n`] };
     const stats = { "tag": "p", "children": [`Seeders: ${stuff.seeder} • Leechers: ${stuff.leecher}\n`] };
-    const links = {
+    const siteLink = {
       "tag": "p", "children": [
         `Size: ${stuff.size}`,
-        // { "tag": "a", "attrs": { "href": '#' }, "children": [' Magnet Link'] },
-        { "tag": "a", "attrs": { "href": stuff.url }, "children": [' Site Link\n'] }
+        { "tag": "a", "attrs": { "href": stuff.url }, "children": [' Site Link '] }
+      ]
+    }
+    const torrentLink = {
+      "tag": "p", "children": [
+        { "tag": "a", "attrs": { "href": stuff.magnet }, "children": [' Magnet Link\n'] },
       ]
     }
 
@@ -61,7 +69,8 @@ async function createPage(responseArray) {
     format.children[0].children.push(title);
     format.children[0].children.push(details);
     format.children[0].children.push(stats);
-    format.children[0].children.push(links);
+    format.children[0].children.push(siteLink);
+    if (stuff.magnet) format.children[0].children.push(torrentLink);
     content.push(format);
   }
   myContent.content = content;
